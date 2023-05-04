0830661b30217b7e9435be0685eb5fb71ed0e5d6
Pouriya Kordaki <pouriya.kordaki@justeattakeaway.com>
2023-05-04 22:47:35 +0200

    init boilderplate

---
+++ includes/class-dafater-report-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://kordaki.net
+ * @since      1.0.0
+ *
+ * @package    Dafater_Report
+ * @subpackage Dafater_Report/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Dafater_Report
+ * @subpackage Dafater_Report/includes
+ * @author     Pouriya Kordaki <pouriya.kordaki@gmail.com>
+ */
+class Dafater_Report_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-dafater-report-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://kordaki.net
+ * @since      1.0.0
+ *
+ * @package    Dafater_Report
+ * @subpackage Dafater_Report/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Dafater_Report
+ * @subpackage Dafater_Report/includes
+ * @author     Pouriya Kordaki <pouriya.kordaki@gmail.com>
+ */
+class Dafater_Report_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
