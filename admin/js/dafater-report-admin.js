02a5892ef842020166db9420467ab5ee6e16545d
Pouriya Kordaki <pouriya.kordaki@justeattakeaway.com>
2023-06-03 03:15:17 +0200

    customized table

--- admin/js/dafater-report-admin.js
+++ admin/js/dafater-report-admin.js
@@ -1,8 +1,12 @@
 jQuery(document).ready(function () {
-  jQuery("#example").DataTable({
+  var $ = jQuery;
+  $("#example").DataTable({
     scrollY: 500,
     scrollX: true,
     scrollCollapse: true,
+    language: {
+      url: "//cdn.datatables.net/plug-ins/1.13.4/i18n/fa.json",
+    },
     // paging: false,
     // fixedColumns: {
     //   left: 1,
